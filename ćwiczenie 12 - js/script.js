39c1faaf024ac129deffd01298e8b8f77a153343
MajaKortyka <maja.kortyka@gmail.com>
2021-08-09 11:23:39 +0200

    ćwiczenie 12 -js

---
+++ ćwiczenie 12 - js/script.js
@@ -0,0 +1,21 @@
+ const "ChopekInfo"= [
+     {
+        src: "https://raw.githubusercontent.com/apietryga/kurs-Front-end/master/materialy/lekcja6/img/gada.gif",
+        alt: "Pan Cyfrowy Gada",
+        desc: "Pan Cyfrowy Gada",
+    }
+     {  src: "https://raw.githubusercontent.com/apietryga/kurs-Front-end/master/materialy/lekcja6/img/siada.gif",
+        alt: "Pan Cyfrowy Siada",
+        desc: "Pan Cyfrowy Siada",
+    }
+     {
+        src: "https://raw.githubusercontent.com/apietryga/kurs-Front-end/master/materialy/lekcja6/img/wstaje.gif",
+        alt: "Pan cyfrowy Wstaje",
+        desc: "Pan cyfrowy Wstaje",
+     }
+      {
+        src: "https://raw.githubusercontent.com/apietryga/kurs-Front-end/master/materialy/lekcja6/img/intro.gif",
+        alt: "Pan Cyfrowy Intro",
+        desc: "Pan Cyfrowy Intro",
+     }
+]
